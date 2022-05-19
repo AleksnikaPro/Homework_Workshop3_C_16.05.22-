2e046811c602e4bd4300d52848f4e95689057e17
Aleksandra Popova <alexnika@bk.ru>
2022-05-19 18:43:40 +0300

    added task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите число A: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите степень, в которую хотите возвести число A: ");
+int numberDegree = int.Parse(Console.ReadLine() ?? "0");
+
+for (int degree = 1; degree <= numberDegree; degree++)
+{
+    Console.WriteLine(Math.Pow(number, degree));
+}
