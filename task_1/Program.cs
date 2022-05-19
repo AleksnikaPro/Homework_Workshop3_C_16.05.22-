ffb6559125eb5adc384869266effcf02096a91e4
Aleksandra Popova <alexnika@bk.ru>
2022-05-19 18:41:08 +0300

    added task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите число n:");
+int number = int.Parse(Console.ReadLine() ?? "0");
+for (int num = 1; num <= number; num++)
+{
+    Console.WriteLine(Math.Pow(num,2));
+}
