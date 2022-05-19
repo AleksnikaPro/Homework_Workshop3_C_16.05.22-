cf7ddd79e40eedd5b7a2ae56e599cd09f327150f
Aleksandra Popova <alexnika@bk.ru>
2022-05-19 18:44:17 +0300

    added task_5

---
+++ task_5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+
+while (number > 0)
+{
+      sum = sum + (number % 10);
+      number = number / 10;
+}
+
+Console.WriteLine($"Сумма цифр в числе {number} равна {sum}");
