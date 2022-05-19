0ec3352040c6bbec4f5832dee9c240ed903e36a1
Aleksandra Popova <alexnika@bk.ru>
2022-05-19 18:43:05 +0300

    added task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите число A:");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+for (int num = 1; num <= number; num++)
+{
+    sum = sum + num;
+}
+Console.WriteLine($"Cумму чисел от 1 до {number} равна {sum} ");
