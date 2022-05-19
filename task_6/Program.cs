dfc07efd7b8a475e4caa796441e411e27cec64e0
Aleksandra Popova <alexnika@bk.ru>
2022-05-19 18:44:46 +0300

    added task_6

---
+++ task_6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int lastDigit = number % 10;
+if(lastDigit % 2 == 0)
+{
+    Console.WriteLine("Введенное Вами число заканчивается на чётную цифру и его можно возвести в куб");
+    Console.WriteLine(Math.Pow(number,3));
+}
+else
+{
+    Console.WriteLine("Введенное Вами число не заканчивается на чётную цифру, поэтому возводить его в куб не будем");
+}
+
