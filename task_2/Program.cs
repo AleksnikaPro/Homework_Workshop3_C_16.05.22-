854ae57c62ba24115f82bb3f41015c1e202ebf92
Aleksandra Popova <alexnika@bk.ru>
2022-05-20 12:24:28 +0300

    added changes in task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -2,7 +2,9 @@
 
 Console.WriteLine("Введите число n:");
 int number = int.Parse(Console.ReadLine() ?? "0");
+int n = 0;
 for (int num = 1; num <= number; num++)
 {
-    Console.WriteLine(Math.Pow(num,3));
+    n++;
+    Console.WriteLine($"Куб числа {n} равен {Math.Pow(num,3)}");
 }
